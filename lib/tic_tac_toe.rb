d4a8af32fd0b6f7d5cdafaa4a124c3d81329bed7
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-10-29 09:36:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,9 +62,8 @@ class TicTacToe
       if (@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X") ||
         (@board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O")
         return combo
-      else
-        return false
       end
     end
+    return false
   end
 end
