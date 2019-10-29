09c2cc492684e93dc58af006e0c373fb8fba288e
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-10-29 09:34:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.each do |combo|
-      if (board[@board[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X") ||
+      if (@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X") ||
         (@board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O")
         return combo
       end
