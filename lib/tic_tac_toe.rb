3d552ee7fdb4d2403a02885802ed82c502aebc65
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-10-29 09:32:27 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,68 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    (input.to_i) - 1
+  end
+
+  def move(index, symbol)
+    @board[index] = symbol
+  end
+
+  def position_taken?(position)
+    @board[position] == "X" || @board[position] == "O"
+  end
+
+  def valid_move?(position)
+    !position_taken?(position) && position.between?(0, 8)
+  end
+
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    puts "Please choose a number 1-9"
+    input = input_to_index(gets.strip)
+    while !valid_move?(input)
+      puts "Invalid input, please choose a number between 1 and 9"
+      input = input_to_index(gets.strip)
+    end
+    move(input, current_player)
+    display_board
+  end
+
+  def won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") ||
+        (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
+        return combo
+      end
+    end
+  end
+end
